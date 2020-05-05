9366056c5c9f3b51774795e2eec77a2d975ed4ec
oskar morell <oskarmorell1998@hotmail.se>
2020-05-05 16:14:44 +0200

    Added Models folder and UserModel

---
+++ FriendFinderAPI/Models/UserModel.cs
@@ -0,0 +1,12 @@
+namespace FriendFinderAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Adress { get; set; }
+        public string Phonenumber { get; set; }
+        public string Hobby { get; set; }
+    }
+}
