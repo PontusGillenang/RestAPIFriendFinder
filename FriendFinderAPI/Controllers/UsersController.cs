743b86916aa3dfe37f581837e7cef0cf6693b29d
Adgnascor <william.lindahl@live.com>
2020-05-05 11:05:42 +0200

    @adgnascor

--- FriendFinderAPI/Controllers/UsersController.cs
+++ FriendFinderAPI/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Microsoft.AspNetCore.Mvc;
+
 namespace FriendFinderAPI.Controllers
 {
-    public class UsersController
+    public class UsersController : ControllerBase
     {
         
     }
