7c3e7183ce64aa9c95bae99e5a81ed3dd9cfc449
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-05-05 15:48:50 +0200

    @bluemace
    Added some more in UserRepository, missing context and model to see if its working or not.

--- FriendFinderAPI/Services/UserRepository.cs
+++ FriendFinderAPI/Services/UserRepository.cs
@@ -1,18 +1,40 @@
 using System.Threading.Tasks;
 //using FriendFinderAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace FriendFinderAPI.Services
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
 
         private readonly UserContext _context;
+
+        public UserRepository(UserContext _context)
+        {
+            _context = context;
+        }
         public async Task<User> GetUser()
         {
 
             var query = _context.User;
             return await query.FirstorDefaultAsync();
         }
+
+
+
+         //missing models and context to test. 
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _context.User.ToList();
+        }
+        
+        //missing models and context to test. 
+        public User GetUserByID(int id)
+        {
+            return _context.User.FirstorDefault(p=> p.UserID == id);
+        }
+
+        
     }
 }
