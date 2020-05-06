46ba92946eda0eac5abdc9b0746639e9a6fa4826
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-05-06 15:11:17 +0200

    @bluemace
    Cleaned up the code.

--- FriendFinderAPI/Context/UserContext.cs
+++ FriendFinderAPI/Context/UserContext.cs
@@ -1,15 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using FriendFinderAPI.Models;
-{
-    
-}
 
 namespace FriendFinderAPI.Context
 {
     public class UserContext : DbContext
     {
-        public DbSet<User> Users {get; set};
+        public DbSet<User> Users {get; set}
         public UserContext(DbContextOptions<UserContext> options) : base (options)
         {
 
