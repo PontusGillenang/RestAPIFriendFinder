f8247cda18a3c850f06259b9bdc56908b1d54f02
Adgnascor <william.lindahl@live.com>
2020-05-07 16:24:38 +0200

    @adgnascor

--- FriendFinderAPI/Models/Match.cs
+++ FriendFinderAPI/Models/Match.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FriendFinderAPI.Models
 {
     public class Match
     {
         public int MatchID { get; set; }
-        public User MatchedUser1 { get; set; }
-        public User MatchedUser2 { get; set; }
+
+        [ForeignKey("UserID1")]
+        [InverseProperty("Matches")]
+        public User MatchedUser1{get;set;}
+
+        [ForeignKey("UserID2")]
+        public User MatchedUser2{get;set;}
     }
 }
