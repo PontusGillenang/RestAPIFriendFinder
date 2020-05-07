c39c63934fde93ca74fbeea9f8814f6e905cdeb0
Adgnascor <william.lindahl@live.com>
2020-05-07 10:31:58 +0200

    @adgnascor  HobbiesController done.
    Made a small change in UsersController (regarding syntax)

--- FriendFinderAPI/Controllers/HobbiesController.cs
+++ FriendFinderAPI/Controllers/HobbiesController.cs
@@ -1,77 +1,75 @@
 using System.Collections.Generic;
 using FriendFinderAPI.Context;
+using FriendFinderAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using FriendFinderAPI.Models;
 
 namespace FriendFinderAPI.Controllers
 {
     [Route("api/v1.0/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class HobbiesController : ControllerBase
     {
         private readonly FriendFinderContext _context;
 
-        public UsersController(FriendFinderContext context) => _context = context;
+        public HobbiesController(FriendFinderContext context) => _context = context;
 
-        //GET:      api/users
+        //GET:      api/v1.0/hobbies
         [HttpGet]
-        public ActionResult<IEnumerable<User>> GetUsers() => _context.Users;
+        public ActionResult<IEnumerable<Hobby>> GetHobbies() => _context.Hobbies;
 
-        //GET:      api/users/n
+        //GET:      api/v1.0/hobbies/n
         [HttpGet("{id}")]
-        public ActionResult<User> GetUserByID(int id)
+        public ActionResult<Hobby> GetHobbyByID(int id)
         {
-            var user = _context.Users.Find(id);
-            if(user == null)
+            var hobby = _context.Hobbies.Find(id);
+            if(hobby == null)
                 return NotFound();
-
-            return user;
+            
+            return hobby;
         }
 
-        //POST:     api/users
+        //POST: api/v1.0/hobbies
         [HttpPost]
-        public ActionResult<User> PostUser(User user)
+        public ActionResult<Hobby> PostHobby(Hobby hobby)
         {
-            _context.Users.Add(user);
+            _context.Hobbies.Add(Hobby);
             //Important to dont forget that save the changes in context when using POST
             _context.SaveChanges();
 
-            return CreatedAtAction("GetUser", new User{UserID = user.UserID}, user);
+            return CreatedAtAction("GetHobby", new Hobby{HobbyID = hobby.HobbyID}, hobby);
         }
 
-        //PUT:      api/users/n
+        //PUT:      api/v1.0/hobbies/n
         [HttpPut("{id}")]
-        public ActionResult PutUser(int id, User user)
+        public ActionResult PutHobby(int id, Hobby hobby)
         {
-            if(id != user.UserID)
+            if(id !=  hobby.HobbyID)
                 return BadRequest();
             
-            _context.Entry(user).State = EntityState.Modified;
+            _context.Entry(hobby).State = EntityState.Modified;
             // Above code line make the changes to we want, in our context,
             // Which means that when we Save context it will save those changes and get rid of previous value
 
             _context.SaveChanges();
-
             // Because of that the changes already been done, we do not need to return any content.
             // That´s why we return method NoContent. So we kinda returns a NoContent object. => Returns a "204 NoContent" Status
 
             return NoContent();
         }
 
-        //DELETE        api/users/n
+        //DELETE:       api/hobbies/n
         [HttpDelete("{id}")]
-        public ActionResult<User> DeleteUser(int id)
+        public ActionResult<Hobby> DeleteHobby(int id)
         {
-            var user = _context.Users.Find(id);
-            if(user == null)
+            var hobby = _context.Hobby.Find(id);
+            if(hobby == null)
                 return NotFound();
             
-            _context.Users.Remove(user);
+            _context.Hobbies.Remove(hobby);
             _context.SaveChanges();
 
-            return user;
+            return hobby;
         }
-        
     }
 }
