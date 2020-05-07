e802ea3f5b0d1a336f1d42cfbee9d8f5775f892f
Adgnascor <william.lindahl@live.com>
2020-05-07 11:06:38 +0200

    @adgnascor created CitiesController

---
+++ FriendFinderAPI/Controllers/CitiesController.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using FriendFinderAPI.Context;
+using FriendFinderAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendFinderAPI.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly FriendFinderContext _context;
+
+        public CitiesController(FriendFinderContext context) => _context = context;
+
+        //GET:      api/v1.0/cities
+        [HttpGet]
+        public ActionResult<IEnumerable<City>> GetCities() => _context.Cities;
+
+        //GET:      api/v1.0/cities/n
+        [HttpGet("{id}")]
+        public ActionResult<City> GetCityByID(int id)
+        {
+            var city = _context.Cities.Find(id);
+            if(city == null)
+                return NotFound();
+            
+            return city;
+        }
+
+        //POST:      api/v1.0/cities
+        [HttpPost]
+        public ActionResult<City> PostCity(City city)
+        {
+            _context.Cities.Add(city);
+            //Important to dont forget that save the changes in context when using POST
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCity", new City{CityID = city.CityID}, city);
+        }
+
+        //PUT:      api/v1.0/cities/n
+        [HttpPut("{id}")]
+        public ActionResult PutCity(int id, City city)
+        {
+            if(id != city.CityID)
+                return BadRequest();
+            
+            _context.Entry(city).State = EntityState.Modified;
+            // Above code line make the changes to we want, in our context,
+            // Which means that when we Save context it will save those changes and get rid of previous value
+
+            _context.SaveChanges();
+
+            // Because of that the changes already been done, we do not need to return any content.
+            // That´s why we return method NoContent. So we kinda returns a NoContent object. => Returns a "204 NoContent" Status
+
+            return NoContent();
+        }
+
+        //DELETE:       api/v1.0/cities/n
+        [HttpDelete("{id}")]
+        public ActionResult<City> DeleteCity(int id)
+        {
+            var city = _context.Cities.Find(id);
+            if(city == null)
+                return NotFound();
+            
+            _context.Cities.Remove(city);
+            _context.SaveChanges();
+
+            return city;
+        }
+        
+    }
+}
