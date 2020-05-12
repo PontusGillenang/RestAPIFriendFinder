56ed26a1dbdbb6d031dfff799fa327aefe56fe21
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-05-12 11:31:08 +0200

    @bluemace Added test for existing Cityrepo methods
    Will need to be expanded when we have more methods in the repo

---
+++ FriendFinderAPI.Tests/UnitTestCities.cs
@@ -0,0 +1,76 @@
+using System;
+using Xunit;
+using System.Collections;
+using System.Collections.Generic;
+using FriendFinderAPI.Context;
+using FriendFinderAPI.Services;
+using FriendFinderAPI.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+
+namespace FriendFinderAPI.Tests
+{
+    public class UnitTestCities
+    {
+        
+    
+        [Fact]
+        public async void GetCitiesTest()
+        {
+           //Arrange
+            IList<City> city = GenerateCities();
+            var FriendFinderContextMock = new Mock<FriendFinderContext>();
+            FriendFinderContextMock.Setup(u=>u.Cities).ReturnsDbSet(city);
+
+            var logger = Mock.Of<ILogger<CityRepository>>();
+            var cityRepository = new CityRepository(FriendFinderContextMock.Object, logger);
+
+            //Act
+            var result = await cityRepository.GetCities();
+
+            //Assert
+            Assert.Equal(2, (int)result.Length);
+        }
+        [Fact]
+        public async void GetCityByIDTest()
+        {
+             //Arrange
+            IList<City> city = GenerateCities();
+            var FriendFinderContextMock = new Mock<FriendFinderContext>();
+            FriendFinderContextMock.Setup(u=>u.Cities).ReturnsDbSet(city);
+
+            var logger = Mock.Of<ILogger<CityRepository>>();
+            var cityRepository = new CityRepository(FriendFinderContextMock.Object, logger);
+
+            //Act
+            var result = await cityRepository.GetCity(2);
+
+            //Assert
+            Assert.Equal(2, result.CityID);
+        }
+        private static IList<City> GenerateCities()
+    {
+        return new List<City>
+        {
+            new City
+            {
+            CityID = 1,
+            CityName ="Göteborg",
+            CityCountry = "Sweden",
+            CityCounty = "Västra Götaland"
+            },
+            new City{
+            CityID = 2,
+            CityName ="Stockholm",
+            CityCountry = "Sweden",
+            CityCounty = "Stockholm"
+            
+            }
+
+        };
+    }
+
+ }
+ }
