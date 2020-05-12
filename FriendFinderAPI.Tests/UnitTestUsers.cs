e3294d6581ee65d5017373679e0c07bb346e1151
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-05-12 10:39:42 +0200

    @bluemace
    Created 2 tests for User Repo, tests getUsers and GetUser. Will need some revamp after we change the naming of user

---
+++ FriendFinderAPI.Tests/UnitTestUsers.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+using System.Collections;
+using System.Collections.Generic;
+using FriendFinderAPI.Context;
+using FriendFinderAPI.Services;
+using FriendFinderAPI.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+
+namespace FriendFinderAPI.Tests
+{
+
+
+    public class UnitTestUsers
+    
+    {
+        [Fact]
+        public async void GetUsersTest()
+        {
+            //Arrange
+            IList<User> users = GenerateUsers();
+            var FriendFinderContextMock = new Mock<FriendFinderContext>();
+            FriendFinderContextMock.Setup(u=>u.Users).ReturnsDbSet(users);
+
+            var logger = Mock.Of<ILogger<UserRepository>>();
+            var userRepository = new UserRepository(FriendFinderContextMock.Object, logger);
+
+            //Act
+            var result = await userRepository.GetAllUsers();
+
+            //Assert
+            Assert.Equal(2, (int)result.Length);
+        }
+
+        [Fact]
+        public async void GetUserByIDTest()
+        {
+            //Arrange
+            IList<User> users = GenerateUsers();
+            var FriendFinderContextMock = new Mock<FriendFinderContext>();
+            FriendFinderContextMock.Setup(u=>u.Users).ReturnsDbSet(users);
+
+            var logger = Mock.Of<ILogger<UserRepository>>();
+            var userRepository = new UserRepository(FriendFinderContextMock.Object, logger);
+
+            //Act
+            var result = await userRepository.GetUser(1);
+
+            //Assert
+            Assert.Equal("Sebbe", result.UserName );
+        }
+
+    private static IList<User> GenerateUsers()
+    {
+        return new List<User>
+        {
+            new User
+            {
+            UserID = 1,
+            UserName = "Sebbe",
+            UserAdress = "Drottninggatan",
+            UserPhoneNumber = "+46XXXXXXX",
+            UserAge = 20,
+            UserIsTeacher = false
+            },
+            new User{
+            UserID = 2,
+            UserName = "Oskar",
+            UserAdress = "Kungsgatan",
+            UserPhoneNumber = "+47XXXXXXX",
+            UserAge = 23,
+            UserIsTeacher = false
+            
+            }
+
+        };
+    }
+}
+
+}
+
+
+
