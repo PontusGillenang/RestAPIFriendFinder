1b052fe74123ea15def36a474f2829bd485b5fae
ChainsGBG <oscar.kempe@learnet.se>
2020-05-12 11:04:04 +0200

    CewlHobbyRepo

---
+++ FriendFinderAPI/Services/HobbyRepository.cs
@@ -0,0 +1,29 @@
+using FriendFinderAPI.Context;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using FriendFinderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendFinderAPI.Services
+{
+    public class HobbyRepository : Repository, IHobbyRepository
+    {
+        public HobbyRepository(FriendFinderContext context, ILogger<HobbyRepository> logger) : base(context, logger)
+        {
+        }
+        public async Task<Hobby> GetHobby(int HobbyID)
+        {
+            _logger.LogInformation($"Getting Hobby with id: {HobbyID}");
+            IQueryable<Hobby> query = _context.Hobbies.Where(u => u.HobbyID == HobbyID);
+
+            return await query.FirstOrDefaultAsync();
+        }
+        async Task<Hobby[]> IHobbyRepository.GetHobbies()
+        {
+            _logger.LogInformation("Getting Hobbies");
+            IQueryable<Hobby> query = _context.Hobbies;
+            return await query.ToArrayAsync();
+        }
+    }
+}
