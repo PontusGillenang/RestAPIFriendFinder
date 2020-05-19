f612b6bbaddaf29ad2dfdcf2ddf20371977a548e
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-05-19 14:37:43 +0200

    Startup.cs fixed with added scopes, newtonsoft etc

--- FriendFinderAPI/Startup.cs
+++ FriendFinderAPI/Startup.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
-using FriendFinderAPI.Models;
+using FriendFinderAPI.Services;
+using AutoMapper;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Formatters.Json;
 using FriendFinderAPI.Context;
 
 namespace FriendFinderAPI
@@ -14,7 +22,25 @@ namespace FriendFinderAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<FriendFinderContext>();
-            services.AddMvc(options => options.EnableEndpointRouting=false).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+            services.AddScoped<ICityRepository, CityRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IHobbyRepository, HobbyRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMvc(options => options.EnableEndpointRouting=false)
+                .SetCompatibilityVersion(Microsoft
+                                        .AspNetCore
+                                        .Mvc
+                                        .CompatibilityVersion
+                                        .Version_3_0);
+
+                services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
