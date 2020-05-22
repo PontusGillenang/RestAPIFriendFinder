bb874da600e8d1097fc15c3940794817c3bfdbfd
Ahmad Yassin <31723305+akyassin@users.noreply.github.com>
2020-05-22 09:13:38 +0200

    CitiesControllerFix

--- FriendFinderAPI/Controllers/CitiesController.cs
+++ FriendFinderAPI/Controllers/CitiesController.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using AutoMapper;
-using FriendFinderAPI.Context;
+using FriendFinderAPI.Dtos;
 using FriendFinderAPI.Models;
+using System.Threading.Tasks;
+using FriendFinderAPI.Context;
+using Microsoft.AspNetCore.Mvc;
 using FriendFinderAPI.Services;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using FriendFinderAPI.Dtos;
 
 
 namespace FriendFinderAPI.Controllers
@@ -21,7 +21,6 @@ namespace FriendFinderAPI.Controllers
         private readonly IMapper _mapper;
         public CitiesController(ICityRepository cityRepository, IMapper mapper)
         {
-            
             _cityRepository = cityRepository;
             _mapper = mapper;
         }
@@ -65,11 +64,12 @@ namespace FriendFinderAPI.Controllers
         }
         
         [HttpGet("hobby")]
-        public async Task<ActionResult<IEnumerable<City>>> GetCitiesByHobby(int id)
+        public async Task<ActionResult<IEnumerable<CityDto>>> GetCitiesByHobby(int id)
         {
             try
             {
                 var results = await _cityRepository.GetCitiesByHobby(id);
+                var mappedResults = _mapper.Map<IEnumerable<CityDto>>(results);
                 return Ok(results);
             }
             catch(Exception e)
@@ -84,11 +84,13 @@ namespace FriendFinderAPI.Controllers
         {
             try
             {
-                var mappedEntity = _mapper.Map<CityDto>(cityDto);
-                _cityRepository.Add(mappedEntity);
+                var mappedEntity = _mapper.Map<City>(cityDto);
 
+                _cityRepository.Add(mappedEntity);
                 if(await _cityRepository.Save())
+                {
                     return Created($"api/v1.0/cities/{mappedEntity.CityID}", _mapper.Map<CityDto>(mappedEntity));
+                }
             }
             catch (Exception e)
             {
@@ -99,14 +101,14 @@ namespace FriendFinderAPI.Controllers
         
 
         //PUT:      api/v1.0/cities/n
-        [HttpPut("{id}")]
-        public async Task<ActionResult<CityDto>> PutCity(int cityID, CityDto cityDto)
+        [HttpPut("{cityId}")]
+        public async Task<ActionResult<CityDto>> PutCity(int cityId, CityDto cityDto)
         {
             try
             {
-                var oldCity = await _cityRepository.GetCity(cityID);
+                var oldCity = await _cityRepository.GetCity(cityId);
                 if(oldCity == null)
-                    return NotFound($"We could not find the city your looking for id: {cityID}");
+                    return NotFound($"We could not find the city your looking for id: {cityId}");
 
                 var newCity = _mapper.Map(cityDto, oldCity);
                 _cityRepository.Update(newCity);
@@ -122,18 +124,22 @@ namespace FriendFinderAPI.Controllers
         }
 
         //DELETE:       api/v1.0/cities/n
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteCity(int cityID)
+        [HttpDelete("{cityId}")]
+        public async Task<ActionResult> DeleteCity(int cityId)
         {
             try
             {
-                var city = await _cityRepository.GetCity(cityID);
+                var city = await _cityRepository.GetCity(cityId);
                 if(city == null)
-                    return NotFound($"Could not find the city with id: {cityID} ");
+                {
+                    return NotFound($"Could not find the city with id: {cityId} ");
+                }
 
                 _cityRepository.Delete(city);
                 if(await _cityRepository.Save())
+                {
                     return NoContent();
+                }
             }
             catch (Exception e)
             {
@@ -141,6 +147,5 @@ namespace FriendFinderAPI.Controllers
             }
             return BadRequest();
         }
-        
     }
 }
