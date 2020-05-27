54d694a33e4778578c1785c1f7118a34024bb99d
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-05-27 14:47:12 +0200

    small change, added SkillLevel to HobbyUserDto

--- FriendFinderAPI/Dtos/HobbyUserDto.cs
+++ FriendFinderAPI/Dtos/HobbyUserDto.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using FriendFinderAPI.Models;
+using static FriendFinderAPI.Enums.Enums;
 
 namespace FriendFinderAPI.Dtos
 {
@@ -13,6 +14,7 @@ namespace FriendFinderAPI.Dtos
         [ForeignKey("UserID")]
         public int UserID { get; set; }
         public UserDto User { get; set; }
+        public UserSkillLevel SkillLevel { get; set; }
         public IEnumerable<Link> Links {get; set;}
     }
 }
