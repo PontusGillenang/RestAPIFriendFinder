5e697b5fc9870529dd3e88e89359f692feffe895
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-05-29 13:47:28 +0200

    Added seed data and other changes for DB

--- FriendFinderAPI/Context/FriendFinderContext.cs
+++ FriendFinderAPI/Context/FriendFinderContext.cs
@@ -83,9 +83,9 @@ namespace FriendFinderAPI.Context
             .OnDelete(DeleteBehavior.NoAction);
             
 
-            string citiesPath = @"../Documentation/DataSets/world-cities.txt";
+            string citiesPath = @"../Documentation/DataSets/cities.txt";
             string[] lines = File.ReadAllLines(citiesPath);
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < 5; i++)
             {
                 string[] split = lines[i].Split(',');
                 City city = new City
@@ -102,59 +102,198 @@ namespace FriendFinderAPI.Context
             .HasData(new
             {
                 UserId = 1,
-                UserName = "Sebbe",
-                UserAdress = "Drottninggatan",
+                UserName = "Sebbe Persson",
+                UserAdress = "Drottninggatan 2",
                 UserPhoneNumber = "+46XXXXXXX",
                 UserAge = 20,
-                CityId = 2
+                CityId = 1
             }
             , new
             {
                 UserId = 2,
-                UserName = "Oskar",
-                UserAdress = "Kungsgatan",
-                UserPhoneNumber = "+46XXXXXXX2",
+                UserName = "Peder Åborg",
+                UserAdress = "Knudsgate 15",
+                UserPhoneNumber = "+45XXXXXXX1",
                 UserAge = 22,
-                CityId = 1
+                CityId = 5
             }
             , new
             {
                 UserId = 3,
-                UserName = "William",
-                UserAdress = "MorTest",
-                UserPhoneNumber = "+46XXXXXXX3",
-                UserAge = 28,
+                UserName = "Petra Levinman",
+                UserAdress = "Karl Andersgatan 6",
+                UserPhoneNumber = "+46XXXXXXX2",
+                UserAge = 24,
+                CityId = 3
+            }
+            , new
+            {
+                UserId = 4,
+                UserName = "Pontus Bergman",
+                UserAdress = "Beringsväg 10",
+                UserPhoneNumber = "+47XXXXXXX3",
+                UserAge = 25,
+                CityId = 4
+            }
+            , new
+            {
+                UserId = 5,
+                UserName = "Olle Kvist",
+                UserAdress = "Kongasnåret 1",
+                UserPhoneNumber = "+47XXXXXXX4",
+                UserAge = 43,
+                CityId = 4
+            }
+            , new
+            {
+                UserId = 6,
+                UserName = "Christina Kerlpalm",
+                UserAdress = "Gregorstande 6",
+                UserPhoneNumber = "+45XXXXXXX5",
+                UserAge = 32,
+                CityId = 5
+            }
+            , new
+            {
+                UserId = 7,
+                UserName = "Osborn Börjesson",
+                UserAdress = "Kungsportsavenyn 3",
+                UserPhoneNumber = "+46XXXXXXX6",
+                UserAge = 34,
                 CityId = 1
+            }
+            , new
+            {
+                UserId = 8,
+                UserName = "Pelle Plutter",
+                UserAdress = "Djurgårdsvägen 21",
+                UserPhoneNumber = "+46XXXXXXX7",
+                UserAge = 26,
+                CityId = 2
+            }
+            , new
+            {
+                UserId = 9,
+                UserName = "Ina Pålström",
+                UserAdress = "Vättlegatan 2",
+                UserPhoneNumber = "+46XXXXXXX8",
+                UserAge = 27,
+                CityId = 2
+            }
+            , new
+            {
+                UserId = 10,
+                UserName = "William Morberg",
+                UserAdress = "Opalgatan 54",
+                UserPhoneNumber = "+46XXXXXXX9",
+                UserAge = 28,
+                CityId = 3
             });
 
             modelBuilder.Entity<Location>()
             .HasData(new
             {
                 LocationId = 1,
-                LocationName = "Fjäderborgen",
-                CityId = 2,
+                LocationName = "Chalmers Tekniska Högskola",
+                CityId = 1,
             }
             , new
             {
                 LocationId = 2,
-                LocationName = "The Castle With Zero Books...",
+                LocationName = "Haga Teatern",
                 CityId = 1,
+            }
+            , new
+            {
+                LocationId = 3,
+                LocationName = "Skansen",
+                CityId = 2,
+            }
+            , new
+            {
+                LocationId = 4,
+                LocationName = "Södermalm",
+                CityId = 2,
+            }
+            , new
+            {
+                LocationId = 5,
+                LocationName = "Stora Torget",
+                CityId = 3,
+            }
+            , new
+            {
+                LocationId = 6,
+                LocationName = "Triangeln",
+                CityId = 3,
+            }
+            , new
+            {
+                LocationId = 7,
+                LocationName = "Kampen",
+                CityId = 4,
+            }
+            , new
+            {
+                LocationId = 8,
+                LocationName = "Domkirke",
+                CityId = 4,
+            }
+            , new
+            {
+                LocationId = 9,
+                LocationName = "Nyhavn",
+                CityId = 5,
+            }
+            , new
+            {
+                LocationId = 10,
+                LocationName = "Nytorv",
+                CityId = 5,
             });
 
             modelBuilder.Entity<Event>()
             .HasData(new
             {
                 EventId = 1,
-                EventName = "Lets Do some Awsome Curling",
-                HobbyId = 2,
+                EventName = "3D Print Workshop",
+                HobbyId = 1,
                 CityId = 1,
             }
             , new
             {
                 EventId = 2,
-                EventName = "BookClub All About The Books",
-                HobbyId = 1,
+                EventName = "Beginners Acting Club",
+                HobbyId = 2,
+                CityId = 1,
+            }
+            , new
+            {
+                EventId = 3,
+                EventName = "Amatuer Airsoft Mayhem",
+                HobbyId = 4,
                 CityId = 2,
+            }
+            , new
+            {
+                EventId = 4,
+                EventName = "Malmö for Biathlon in the World",
+                HobbyId = 8,
+                CityId = 3,
+            }
+            , new
+            {
+                EventId = 5,
+                EventName = "Nightime in Oslo with The Stars",
+                HobbyId = 6,
+                CityId = 4,
+            }
+            , new
+            {
+                EventId = 6,
+                EventName = "Danske Ølefest me Beer Pong",
+                HobbyId = 7,
+                CityId = 5,
             });
 
             modelBuilder.Entity<HobbyLocation>()
@@ -167,21 +306,87 @@ namespace FriendFinderAPI.Context
             {
                 HobbyId = 2,
                 LocationId = 2,
+            }
+            , new
+            {
+                HobbyId = 4,
+                LocationId = 3,
+            }
+            , new
+            {
+                HobbyId = 6,
+                LocationId = 6,
+            }
+            , new
+            {
+                HobbyId = 7,
+                LocationId = 9,
+            }
+            , new
+            {
+                HobbyId = 8,
+                LocationId = 6,
             });
 
             modelBuilder.Entity<EventUser>()
             .HasData(new
             {
-                EventUserId = 1,
                 EventId = 1,
-                UserId = 2,
+                UserId = 1,
                 UserIsResponsible = true
             }
             , new
             {
-                EventUserId = 2,
                 EventId = 1,
-                UserId = 1,
+                UserId = 2,
+                UserIsResponsible = false
+            }
+            , new
+            {
+                EventId = 3,
+                UserId = 3,
+                UserIsResponsible = true
+            }
+            , new
+            {
+                EventId = 3,
+                UserId = 4,
+                UserIsResponsible = false
+            }
+            , new
+            {
+                EventId = 4,
+                UserId = 5,
+                UserIsResponsible = true
+            }
+            , new
+            {
+                EventId = 4,
+                UserId = 6,
+                UserIsResponsible = false
+            }
+            , new
+            {
+                EventId = 5,
+                UserId = 7,
+                UserIsResponsible = true
+            }
+            , new
+            {
+                EventId = 5,
+                UserId = 8,
+                UserIsResponsible = false
+            }
+            , new
+            {
+                EventId = 6,
+                UserId = 9,
+                UserIsResponsible = true
+            }
+            , new
+            {
+                EventId = 6,
+                UserId = 10,
                 UserIsResponsible = false
             });
 
@@ -190,24 +395,66 @@ namespace FriendFinderAPI.Context
             {
                 UserId = 1,
                 HobbyId = 1,
-                SkillLevel = UserSkillLevel.Beginner
+                SkillLevel = UserSkillLevel.Master
             }
             , new
             {
                 UserId = 2,
-                HobbyId = 2,
+                HobbyId = 1,
                 SkillLevel = UserSkillLevel.Intermediate
             }
             , new
             {
                 UserId = 3,
                 HobbyId = 3,
-                SkillLevel = UserSkillLevel.Master
+                SkillLevel = UserSkillLevel.Advanced
+            }
+            , new
+            {
+                UserId = 4,
+                HobbyId = 3,
+                SkillLevel = UserSkillLevel.Beginner
+            }
+            , new
+            {
+                UserId = 5,
+                HobbyId = 8,
+                SkillLevel = UserSkillLevel.Intermediate
+            }
+            , new
+            {
+                UserId = 6,
+                HobbyId = 8,
+                SkillLevel = UserSkillLevel.Beginner
+            }
+            , new
+            {
+                UserId = 7,
+                HobbyId = 6,
+                SkillLevel = UserSkillLevel.Beginner
+            }
+            , new
+            {
+                UserId = 8,
+                HobbyId = 6,
+                SkillLevel = UserSkillLevel.Advanced
+            }
+            , new
+            {
+                UserId = 9,
+                HobbyId = 7,
+                SkillLevel = UserSkillLevel.Advanced
+            }
+            , new
+            {
+                UserId = 10,
+                HobbyId = 7,
+                SkillLevel = UserSkillLevel.Intermediate
             });
 
             string hobbiesPath = @"../Documentation/DataSets/HobbiesList.txt";
             string[] hobbies = File.ReadAllLines(hobbiesPath);
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < 10; i++)
             {
                 Hobby hobby = new Hobby
                 {
