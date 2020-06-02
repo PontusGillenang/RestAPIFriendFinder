ecfae08c7acec48117d349c89cf9e3e42ac9ae1e
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-06-02 15:41:25 +0200

    Added RequestRateLimit, not done for production but working

--- FriendFinderAPI/Startup.cs
+++ FriendFinderAPI/Startup.cs
@@ -10,11 +10,20 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using Microsoft.OpenApi.Models;
 using System.Collections.Generic;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Http;
+using FriendFinderAPI;
+using Microsoft.Extensions.Configuration;
 
 namespace FriendFinderAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<FriendFinderContext>();
@@ -59,11 +68,38 @@ namespace FriendFinderAPI
                 });
             });
 
-                services.AddControllers()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                });
+            services.AddControllers()
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            // needed to load configuration from appsettings.json
+            services.AddOptions();
+
+            // needed to store rate limit counters and ip rules
+            services.AddMemoryCache();
+
+            //load general configuration from appsettings.json
+            services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"));
+
+            //load ip rules from appsettings.json
+            services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"));
+
+            // inject counter and rules stores
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+            // Add framework services.
+            services.AddMvc();
+
+            // https://github.com/aspnet/Hosting/issues/793
+            // the IHttpContextAccessor service is not registered by default.
+            // the clientId/clientIp resolvers use it.
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // configuration (resolvers, counter key builders)
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -80,6 +116,8 @@ namespace FriendFinderAPI
                 configure.SwaggerEndpoint("/swagger/v1/swagger.json", "FriendFinderAPI v1");
  
             });
+            
+            app.UseIpRateLimiting();
 
             app.UseMvc();
         }
