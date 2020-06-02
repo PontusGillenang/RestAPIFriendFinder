9474bab3dfe73b0248a94968fb26bf56441580e8
PontusGillenang <PontusGillenang@users.noreply.github.com>
2020-06-02 13:40:56 +0200

    Added EventTime
    Added two new properties EventStart and EventEnd. Made a new migration aswell.

--- FriendFinderAPI/Dtos/EventDto.cs
+++ FriendFinderAPI/Dtos/EventDto.cs
@@ -1,21 +1,28 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using FriendFinderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FriendFinderAPI.Dtos
 {
     public class EventDto
     {
-        public int EventId {get;set;}
-        public string EventName {get;set;}
+        [Required]
+        public int EventId { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        public string EventName { get; set; }
+        public DateTime EventStart { get; set; }
+        public DateTime EventEnd { get; set; }
 
         [ForeignKey("HobbyId")]
         public int HobbyId { get; set; }
-        public HobbyDto Hobby {get;set;}
-        
+        public HobbyDto Hobby { get; set; }
+
         [ForeignKey("CityId")]
         public int CityId { get; set; }
-        public CityDto City { get;set; }
+        public CityDto City { get; set; }
         public ICollection<EventUserDto> EventUsers { get; set; }
         public IEnumerable<Link> Links { get; set; }
     }
